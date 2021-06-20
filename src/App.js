057569a257817180e468daf90e6e9ca973dbad09
Thiago Orlandini <thiagorlandini@hotmail.com>
2021-06-20 12:58:59 -0300

    final primeiro bloco

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from './Header'
 
-function App() {
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const clientes = [luana, mario];
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Header/>
+      <p>Clientes</p>
+      {clientes.map(cliente=>{
+
+        let colorStyle = cliente.ativa ? 'green': 'red';
+        
+        let total = cliente.compras.map(compra => Number(compra.preco.replace('R$ ', ''))).reduce((a,b) => a+b);
+
+        // Se o gasto for maior que 10000 mostre uma mensagem
+
+        return (
+          <div key={cliente.cliente}>
+            <p style={{color: colorStyle}}>{cliente.cliente} idade: {cliente.idade}</p>
+            {total > 10000 && <p>'Total de compras maior que R$10.000'</p>}
+          </div>
+        )
+
+      })}
+  </div>)
+};
 
 export default App;
